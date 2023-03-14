f99888058dd115a8042b7d1154210d94f989c51a
windofwind <windofwind@gmail.com>
2023-03-14 22:41:37 +0900

    🚨 린터 룰 추가.

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,6 +30,7 @@ module.exports = {
     '@typescript-eslint/no-inferrable-types': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
     'no-nested-ternary': 'error',
     'no-unexpected-multiline': 'error',
     // '@typescript-eslint/no-misused-promises': 'error',
